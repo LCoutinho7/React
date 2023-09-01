b72d8678bfa07e2d137990f41af3fb56673767c0
LCoutinho7 <rm551617@fiap.com.br>
2023-08-31 23:19:13 -0300

    Criando Págs Home, Erro, Produtos e EditarProdutos

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './assets/routes/Home/index.jsx'
+import Produtos from './assets/routes/Produtos/index.jsx'
+import Error from './assets/routes/Error/index.jsx'
+import EditarProduto from './assets/routes/EditarProduto/index.jsx'
+
+import React, { Children } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
+
+const router = createBrowserRouter({
+  path: "/",
+  element: <App/>,
+  errorElement: <Error/>,
+  children:[
+    {
+      path: "/",
+      element: <Home/>,
+    },
+    {
+      path: "/produtos",
+      element: <Produtos/>,
+    },
+    {
+      path: "/produtos/editar/:id",
+      element: <EditarProduto/>,
+    },
+  ]
+
+})
